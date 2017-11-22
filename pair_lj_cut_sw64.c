0c562171c7b64eb524bbb4a5fca06109f023c056
Workspace <1933375079@qq.com>
2017-11-22 16:10:03 +0800

    faster vec

--- pair_lj_cut_sw64.c
+++ pair_lj_cut_sw64.c
@@ -45,7 +45,7 @@ void pair_lj_cut_sunway_compute(compute_param_t *pm){
 }
 #endif
 #ifdef CPE
-#define LWPF_KERNELS _K(COMP) K(CACHE) K(IREAD) K(IWRITE) K(ALL) K(GST) K(JLOOP)
+#define LWPF_KERNELS _K(COMP) K(CACHE) K(IREAD) K(IWRITE) K(ALL) K(GST) K(VECPACK) K(JLOOP)
 #define LWPF_UNIT U(LJCUT)
 #include "lwpf.h"
 #include <dma.h>
@@ -139,12 +139,19 @@ static inline void reg_reduce_inplace_doublev4(doublev4 *arr, int len){
     ot2 = simd_vshff(o3,o1,0x88);                               \
     ot3 = simd_vshff(o3,o1,0xDD);                               \
   }
+#define transpose4x4_2x4(in0, in1, in2, in3, ot0, ot1) {                \
+    doublev4 o0 = simd_vshff(in1,in0,0x44);                             \
+    doublev4 o2 = simd_vshff(in3,in2,0x44);                             \
+    ot0 = simd_vshff(o2,o0,0x88);                                       \
+    ot1 = simd_vshff(o2,o0,0xDD);                                       \
+  }
+
 #define vshuffd_rc(a, b, c, d) (d | (c << 2) | (b << 4) | (a << 6))
 #define simd_vsumd(x) {                                 \
     x += simd_vshff(x, x, vshuffd_rc(2, 3, 0, 1));      \
     x += simd_vshff(x, x, vshuffd_rc(1, 0, 3, 2));      \
   } 
-#define JLIST_PAGESIZE 64
+#define JLIST_PAGESIZE 128
 #define ILIST_PAGESIZE 128
 
 #define JCACHE_HBIT 10
@@ -380,9 +387,14 @@ void pair_lj_cut_sunway_compute_para(compute_param_t *pm){
   lwpf_stop(ALL);
 }
 
+__thread_local volatile int cache_reply;            
 void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
   lwpf_start(ALL);
   pe_init();
+  doublev4 v4_1 = 1.0;
+  doublev4 v4_0 = 0;
+  doublev4 v4_half = 0.5;
+
   int i,j,ii,jj,inum,jnum,itype,jtype;
   double xtmp,ytmp,ztmp,delx,dely,delz,evdwl,fpair;
   double rsq,r2inv,r6inv,forcelj,factor_lj;
@@ -402,7 +414,8 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
 
   compute_param_t l_pm;
   pe_get(pm, &l_pm, sizeof(compute_param_t));
-  double INF = 1.0 / 0.0;
+  //double INF = 1.0 / 0.0;
+  doublev4 INF_v4 = simd_set_doublev4(1e8, 1e8, 1e8, 0);
   pe_syn();
   int ntp1 = l_pm.ntypes + 1;
   double cutsq[ntp1][ntp1], lj1[ntp1][ntp1], lj2[ntp1][ntp1], lj3[ntp1][ntp1], lj4[ntp1][ntp1], offset[ntp1][ntp1];
@@ -436,12 +449,11 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
   atom_in_t jlist_atom[JLIST_PAGESIZE];
 
   doublev4 padding_v4_0, padding_v4_1;
-  doublev4 jlist_x_v4[JLIST_PAGESIZE >> 2][3];
-  doublev4 jlist_lj_v4[JLIST_PAGESIZE >> 2][4];
-  doublev4 jlist_cutsq_v4[JLIST_PAGESIZE >> 2];
-  doublev4 jlist_offset_v4[JLIST_PAGESIZE >> 2];
-  doublev4 jlist_flj_v4[JLIST_PAGESIZE >> 2];
-  //int tsb[8];
+  doublev4 jlist_x_v4[3];
+  doublev4 jlist_lj_v4[4];
+  doublev4 jlist_cutsq_v4;
+  doublev4 jlist_offset_v4;
+  doublev4 jlist_flj_v4;
 
   atom_in_t j_cache[JCACHE_LINECNT][JCACHE_LINESIZE];
   int jcache_tag[JCACHE_LINECNT];
@@ -451,14 +463,15 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
   double ei[ILIST_PAGESIZE], vi[ILIST_PAGESIZE][6], v[6];
   int ti[ILIST_PAGESIZE], *fn[ILIST_PAGESIZE], nn[ILIST_PAGESIZE];
   int ipage_start;
-  volatile int cache_reply;
-  dma_desc cache_get_desc = 0;
-  //memset(&cache_get_desc, 0, sizeof(dma_desc));
+
+  volatile dma_desc cache_get_desc = 0;
+
   dma_set_mode(&cache_get_desc, PE_MODE);
   dma_set_size(&cache_get_desc, sizeof(atom_in_t) * JCACHE_LINESIZE);
   dma_set_op(&cache_get_desc, DMA_GET);
   dma_set_reply(&cache_get_desc, &cache_reply);
-  lwpf_start(COMP);
+
+  //lwpf_start(COMP);
   for (i = 0; i < JCACHE_LINECNT; i ++)
     jcache_tag[i] = -1;
   for (ipage_start = ILIST_PAGESIZE * _MYID; ipage_start < inum; ipage_start += ILIST_PAGESIZE * 64){
@@ -466,14 +479,14 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
     if (ipage_end > inum)
       ipage_end = inum;
     int ipage_size = ipage_end - ipage_start;
-    lwpf_start(IREAD);
+    //lwpf_start(IREAD);
     pe_get(x[ipage_start], xi, ipage_size * sizeof(double) * 3);
     pe_get(type + ipage_start, ti, ipage_size * sizeof(int));
     pe_get(firstneigh + ipage_start, fn, ipage_size * sizeof(int*));
     pe_get(numneigh + ipage_start, nn, ipage_size * sizeof(int));
 
     pe_syn();
-    lwpf_stop(IREAD);
+    //lwpf_stop(IREAD);
     for (ii = ipage_start; ii < ipage_end; ii ++) {
       i = ii;
       int ioff = i - ipage_start;
@@ -487,153 +500,87 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
       //ei[ioff] = 0;
       //vi[ioff][0] = vi[ioff][1] = vi[ioff][2] = 0;
       //vi[ioff][3] = vi[ioff][4] = vi[ioff][5] = 0;
+      doublev4 xi_v4[3];
+      xi_v4[0] = xi[ioff][0];
+      xi_v4[1] = xi[ioff][1];
+      xi_v4[2] = xi[ioff][2];
+
       itype = ti[ioff];
       jlist = fn[ioff];
       jnum = nn[ioff];
-      int jpage_start;
+      int jpage_start, jpage_size;
       for (jpage_start = 0; jpage_start < jnum; jpage_start += JLIST_PAGESIZE){
-        int jpage_size = JLIST_PAGESIZE;
+        jpage_size = JLIST_PAGESIZE;
         if (JLIST_PAGESIZE + jpage_start > jnum)
           jpage_size = jnum - jpage_start;
         pe_get(jlist + jpage_start, jlist_buf, jpage_size * sizeof(int));
         pe_syn();
         
-        lwpf_start(CACHE);
-        for (jj = 0; jj < jpage_size; jj++) {
-          j = jlist_buf[jj];
-          int sbj = sbmask(j);
-          j &= NEIGHMASK;
-          int line = j >> JCACHE_SBIT & JCACHE_LMASK;
-          int tag = j >> JCACHE_HBIT;
+        int jpage_size_to = jpage_size & ~3;
 
-          if (jcache_tag[line] != tag){
-            int mem = j & ~JCACHE_MMASK;
-            //cache_reply = 0;
-            //dma(cache_get_desc, l_pm.atom_in + mem, j_cache[line]);
-            //while (cache_reply != 1);
-            pe_get(l_pm.atom_in + mem, j_cache[line], sizeof(atom_in_t) * JCACHE_LINESIZE);
-            pe_syn();
-
-            jcache_tag[line] = tag;
+        for (jj = 0; jj < jpage_size_to; jj+=4) {
+          int sbj[4];
+          int jjj;
+          int flj_msk = 0;
+          doublev4 xjv[4];
+          for (jjj = 0; jjj < 4; jjj ++){
+            j = jlist_buf[jj + jjj];
+            sbj[jjj] = sbmask(j);
+            j &= NEIGHMASK;
+            int line = j >> JCACHE_SBIT & JCACHE_LMASK;
+            int tag = j >> JCACHE_HBIT;
+            flj_msk |= sbj[jjj] << (jjj * 2);
+            if (jcache_tag[line] != tag){
+              int mem = j & ~JCACHE_MMASK;
+              cache_reply = 0;
+              dma(cache_get_desc, l_pm.atom_in + mem, j_cache[line]);
+              while (cache_reply != 1);
+              jcache_tag[line] = tag;
+            }
+            simd_load(xjv[jjj], j_cache[line] + (j & JCACHE_MMASK));
           }
-          jlist_atom[jj] = j_cache[line][j & JCACHE_MMASK];
-          jlist_atom[jj].sbj = sbj;
-        }
-        lwpf_stop(CACHE);
-        for (jj = jpage_size; jj < ((jpage_size + 3) & (~3)); jj ++){
-          jlist_atom[jj].x[0] = 1e8;
-          jlist_atom[jj].sbj = 0;
-          jlist_atom[jj].type = 0;
-        }
 
-        for (jj = 0; jj < jpage_size; jj += 4){
-          int jv = jj >> 2;
-          doublev4 xj0, xj1, xj2, xj3;
-          simd_load(xj0, jlist_atom + jj + 0);
-          simd_load(xj1, jlist_atom + jj + 1);
-          simd_load(xj2, jlist_atom + jj + 2);
-          simd_load(xj3, jlist_atom + jj + 3);
-          transpose4x4(xj0, xj1, xj2, xj3,
-                       jlist_x_v4[jv][0], jlist_x_v4[jv][1], jlist_x_v4[jv][2],
+          transpose4x4(xjv[0], xjv[1], xjv[2], xjv[3],
+                       jlist_x_v4[0], jlist_x_v4[1], jlist_x_v4[2],
                        padding_v4_0);
 
           int *tsb = &padding_v4_0;
-          int t0 = tsb[0]; //jlist_atom[jj + 0].type; //simd_vextw0(padding_v4_0);
-          int t1 = tsb[2]; //jlist_atom[jj + 1].type; //simd_vextw2(padding_v4_0);
-          int t2 = tsb[4]; //jlist_atom[jj + 2].type; //simd_vextw4(padding_v4_0);
-          int t3 = tsb[6]; //jlist_atom[jj + 3].type; //simd_vextw6(padding_v4_0);
+          int t0 = tsb[0];
+          int t1 = tsb[2];
+          int t2 = tsb[4];
+          int t3 = tsb[6];
 
           int it = itype;
           transpose4x4(lj_v4[it][t0], lj_v4[it][t1], lj_v4[it][t2], lj_v4[it][t3],
-                       jlist_lj_v4[jv][0], jlist_lj_v4[jv][1],
-                       jlist_lj_v4[jv][2], jlist_lj_v4[jv][3]);
+                       jlist_lj_v4[0], jlist_lj_v4[1],
+                       jlist_lj_v4[2], jlist_lj_v4[3]);
+          transpose4x4_2x4(csqoff_v4[it][t0], csqoff_v4[it][t1],
+                           csqoff_v4[it][t2], csqoff_v4[it][t3],
+                           jlist_cutsq_v4, jlist_offset_v4);
 
-          int sbj0 = tsb[1]; //jlist_atom[jj + 0].sbj; //simd_vextw1(padding_v4_0);
-          int sbj1 = tsb[3]; //jlist_atom[jj + 1].sbj; //simd_vextw3(padding_v4_0);
-          int sbj2 = tsb[5]; //jlist_atom[jj + 2].sbj; //simd_vextw5(padding_v4_0);
-          int sbj3 = tsb[7]; //jlist_atom[jj + 3].sbj; //simd_vextw7(padding_v4_0);
-          jlist_flj_v4[jv] = simd_vshff(slj_v4, slj_v4,
-                                        sbj0 | (sbj1 << 2) | (sbj2 << 4) | (sbj3 << 6));
-          transpose4x4(csqoff_v4[it][t0], csqoff_v4[it][t1],
-                       csqoff_v4[it][t2], csqoff_v4[it][t3],
-                       jlist_cutsq_v4[jv], jlist_offset_v4[jv], padding_v4_0, padding_v4_1);
-
-        }
-        /* for (jj = 0; jj < jpage_size; jj += 4){ */
-        /*   int jv = jj >> 2; */
-        /*   doublev4 xj0, xj1, xj2, xj3; */
-        /*   simd_load(xj0, jlist_atom + jj + 0); */
-        /*   simd_load(xj1, jlist_atom + jj + 1); */
-        /*   simd_load(xj2, jlist_atom + jj + 2); */
-        /*   simd_load(xj3, jlist_atom + jj + 3); */
-        /*   transpose4x4(xj0, xj1, xj2, xj3, */
-        /*                jlist_x_v4[jv][0], jlist_x_v4[jv][1], jlist_x_v4[jv][2], */
-        /*                padding_v4_0); */
-        /*   //simd_store(padding_v4_0, tsb); */
-        /*   int *tsb = padding_v4_0; */
-        /*   int t0 = 1; //jlist_atom[jj + 0].type; //simd_vextw0(padding_v4_0); */
-        /*   int t1 = 1; //jlist_atom[jj + 1].type; //simd_vextw2(padding_v4_0); */
-        /*   int t2 = 1; //jlist_atom[jj + 2].type; //simd_vextw4(padding_v4_0); */
-        /*   int t3 = 1; //jlist_atom[jj + 3].type; //simd_vextw6(padding_v4_0); */
-
-        /*   int it = itype; */
-        /*   transpose4x4(lj_v4[it][t0], lj_v4[it][t1], lj_v4[it][t2], lj_v4[it][t3], */
-        /*                jlist_lj_v4[jv][0], jlist_lj_v4[jv][1], */
-        /*                jlist_lj_v4[jv][2], jlist_lj_v4[jv][3]); */
-        /*   int sbj0 = 0; //jlist_atom[jj + 0].sbj; //simd_vextw1(padding_v4_0); */
-        /*   int sbj1 = 0; //jlist_atom[jj + 1].sbj; //simd_vextw3(padding_v4_0); */
-        /*   int sbj2 = 0; //jlist_atom[jj + 2].sbj; //simd_vextw5(padding_v4_0); */
-        /*   int sbj3 = 0; //jlist_atom[jj + 3].sbj; //simd_vextw7(padding_v4_0); */
-        /*   jlist_flj_v4[jv] = simd_vshff(slj_v4, slj_v4, */
-        /*                                 sbj0 | (sbj1 << 2) | (sbj2 << 4) | (sbj3 << 6)); */
-        /*   transpose4x4(csqoff_v4[it][t0], csqoff_v4[it][t1], */
-        /*                csqoff_v4[it][t2], csqoff_v4[it][t3], */
-        /*                jlist_cutsq_v4[jv], jlist_offset_v4[jv], padding_v4_0, padding_v4_1); */
-        /* } */
-        lwpf_start(JLOOP);
-        int jpage_size_v4 = (jpage_size + 3) >> 2;
-        doublev4 xi_v4[3];
-        xi_v4[0] = xi[ioff][0];
-        xi_v4[1] = xi[ioff][1];
-        xi_v4[2] = xi[ioff][2];
-        doublev4 v4_1 = 1.0;
-        doublev4 v4_0 = 0;
-        doublev4 v4_half = 0.5;
-        for (jj = 0; jj < jpage_size_v4; jj ++){
-          doublev4 delx_v4 = xi_v4[0] - jlist_x_v4[jj][0];
-          doublev4 dely_v4 = xi_v4[1] - jlist_x_v4[jj][1];
-          doublev4 delz_v4 = xi_v4[2] - jlist_x_v4[jj][2];
+          jlist_flj_v4 = simd_vshff(slj_v4, slj_v4, flj_msk);
+          doublev4 delx_v4 = xi_v4[0] - jlist_x_v4[0];
+          doublev4 dely_v4 = xi_v4[1] - jlist_x_v4[1];
+          doublev4 delz_v4 = xi_v4[2] - jlist_x_v4[2];
           doublev4 rsq_v4 = delx_v4 * delx_v4 + dely_v4 * dely_v4 + delz_v4 * delz_v4;
           
-          doublev4 msk_v4 = -simd_vfcmplt(rsq_v4, jlist_cutsq_v4[jj]);
+          doublev4 msk_v4 = simd_vfcmple(jlist_cutsq_v4, rsq_v4);
           doublev4 r2inv_v4 = v4_1 / rsq_v4;
-          r2inv_v4 = simd_vsellt(msk_v4, r2inv_v4, v4_0);
+          //r2inv_v4 = simd_vsellt(msk_v4, r2inv_v4, v4_0);
           doublev4 r6inv_v4 = r2inv_v4 * r2inv_v4 * r2inv_v4;
-          doublev4 forcelj_v4 = r6inv_v4 * (jlist_lj_v4[jj][0] * r6inv_v4 - jlist_lj_v4[jj][1]);
-          doublev4 fpair_v4 = jlist_flj_v4[jj] * forcelj_v4 * r2inv_v4;
-          fpair_v4 = simd_vsellt(msk_v4, fpair_v4, v4_0);
-          delx_v4 = simd_vsellt(msk_v4, delx_v4, v4_0);
+          doublev4 forcelj_v4 = r6inv_v4 * (jlist_lj_v4[0] * r6inv_v4 - jlist_lj_v4[1]);
+          doublev4 fpair_v4 = jlist_flj_v4 * forcelj_v4 * r2inv_v4;
+          fpair_v4 = simd_vselle(msk_v4, fpair_v4, v4_0);
+          //delx_v4 = simd_vsellt(msk_v4, delx_v4, v4_0);
           
           fi_v4[0] += delx_v4 * fpair_v4;
           fi_v4[1] += dely_v4 * fpair_v4;
           fi_v4[2] += delz_v4 * fpair_v4;
-          doublev4 v_fac = simd_vsellt(msk_v4, v4_half, v4_0);
+          doublev4 v_fac = simd_vselle(msk_v4, v4_half, v4_0);
           if (l_pm.evflag) {
             if (l_pm.eflag_either){
-              doublev4 evdwl_v4 = r6inv_v4 * (jlist_lj_v4[jj][2] * r6inv_v4 - jlist_lj_v4[jj][3]) - jlist_offset_v4[jj];
-              evdwl_v4 *= jlist_flj_v4[jj];
-              if (_MYID == 0){
-                /* simd_print_doublev4(evdwl_v4); */
-                /* simd_print_doublev4(eng_vdwl_v4); */
-                /* simd_print_doublev4(jlist_flj_v4[jj]); */
-                /* simd_print_doublev4(jlist_lj_v4[jj][2]); */
-                /* simd_print_doublev4(jlist_lj_v4[jj][3]); */
-                /* simd_print_doublev4(jlist_offset_v4[jj]); */
-                /* simd_print_doublev4(r6inv_v4); */
-                /* simd_print_doublev4(r2inv_v4); */
-                /* simd_print_doublev4(rsq_v4); */
-                /* puts(""); */
-              }
+              doublev4 evdwl_v4 = r6inv_v4 * (jlist_lj_v4[2] * r6inv_v4 - jlist_lj_v4[3]) - jlist_offset_v4;
+              evdwl_v4 *= jlist_flj_v4;
               if (l_pm.eflag_global)
                 eng_vdwl_v4 += v_fac * evdwl_v4;
               if (l_pm.eflag_atom) ei_v4 += v_fac * evdwl_v4;
@@ -665,7 +612,6 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
             }
           }
         }
-        lwpf_stop(JLOOP);
       }
 
       simd_vsumd(fi_v4[0]);
@@ -694,17 +640,83 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
 
       ei[ioff] = ei_v4;
 
+      for (jj = jpage_size & ~3; jj < jpage_size; jj++) {
+        j = jlist_buf[jj];
+        factor_lj = special_lj[sbmask(j)];
+        j &= NEIGHMASK;
+        int line = j >> JCACHE_SBIT & JCACHE_LMASK;
+        int tag = j >> JCACHE_HBIT;
+
+        if (jcache_tag[line] != tag){
+          int mem = j & ~JCACHE_MMASK;
+          cache_reply = 0;
+          dma(cache_get_desc, l_pm.atom_in + mem, j_cache[line]);
+          while (cache_reply != 1);
+          jcache_tag[line] = tag;
+        }
+        
+        delx = xi[ioff][0] - j_cache[line][j & JCACHE_MMASK].x[0];
+        dely = xi[ioff][1] - j_cache[line][j & JCACHE_MMASK].x[1];
+        delz = xi[ioff][2] - j_cache[line][j & JCACHE_MMASK].x[2];
+        jtype = j_cache[line][j & JCACHE_MMASK].type;
+        rsq = delx * delx + dely * dely + delz * delz;
+        
+        if (rsq < cutsq[itype][jtype]){
+          r2inv = 1 / rsq;
+          r6inv = r2inv * r2inv * r2inv;
+          forcelj = r6inv * (lj1[itype][jtype] * r6inv - lj2[itype][jtype]);
+          fpair = factor_lj * forcelj * r2inv;
+          
+          fi[ioff][0] += delx * fpair;
+          fi[ioff][1] += dely * fpair;
+          fi[ioff][2] += delz * fpair;
+          if (l_pm.evflag) {
+            if (l_pm.eflag_either){
+              double evdwl = r6inv * (lj3[itype][jtype] * r6inv - lj4[itype][jtype]) - offset[itype][jtype];
+              evdwl *= factor_lj;
+              if (l_pm.eflag_global)
+                *eng_vdwl += 0.5 * evdwl;
+              if (l_pm.eflag_atom) ei[ioff] += 0.5 * evdwl;
+            }
+            if (l_pm.vflag_either) {
+              double v[6];
+              v[0] = 0.5 * delx * delx * fpair;
+              v[1] = 0.5 * dely * dely * fpair;
+              v[2] = 0.5 * delz * delz * fpair;
+              v[3] = 0.5 * delx * dely * fpair;
+              v[4] = 0.5 * delx * delz * fpair;
+              v[5] = 0.5 * dely * delz * fpair;
+              if (l_pm.vflag_global) {
+                virial[0] += v[0];
+                virial[1] += v[1];
+                virial[2] += v[2];
+                virial[3] += v[3];
+                virial[4] += v[4];
+                virial[5] += v[5];
+              }
+              if (l_pm.vflag_atom) {
+                vi[ioff][0] += v[0];
+                vi[ioff][1] += v[1];
+                vi[ioff][2] += v[2];
+                vi[ioff][3] += v[3];
+                vi[ioff][4] += v[4];
+                vi[ioff][5] += v[5];
+              }
+            }
+          }
+        }
+      }
     }
-    lwpf_start(IWRITE);
+    //lwpf_start(IWRITE);
     pe_put(f[ipage_start], fi[0], sizeof(double) * 3 * ipage_size);
     if (l_pm.eflag_either && l_pm.eflag_atom)
       pe_put(l_pm.eatom + ipage_start, ei, sizeof(double) * ipage_size);
     if (l_pm.vflag_either && l_pm.vflag_atom)
       pe_put(l_pm.vatom[ipage_start], vi[0], sizeof(double) * 6 * ipage_size);
     pe_syn();
-    lwpf_stop(IWRITE);
+    //lwpf_stop(IWRITE);
   }
-  lwpf_stop(COMP);
+  //lwpf_stop(COMP);
   simd_vsumd(eng_vdwl_v4);
   simd_vsumd(eng_coul_v4);
   simd_vsumd(virial_v4[0]);
@@ -714,14 +726,14 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
   simd_vsumd(virial_v4[4]);
   simd_vsumd(virial_v4[5]);
 
-  *eng_vdwl = eng_vdwl_v4;
-  *eng_coul = eng_coul_v4;
-  virial[0] = virial_v4[0];
-  virial[1] = virial_v4[1];
-  virial[2] = virial_v4[2];
-  virial[3] = virial_v4[3];
-  virial[4] = virial_v4[4];
-  virial[5] = virial_v4[5];
+  *eng_vdwl += eng_vdwl_v4;
+  *eng_coul += eng_coul_v4;
+  virial[0] += virial_v4[0];
+  virial[1] += virial_v4[1];
+  virial[2] += virial_v4[2];
+  virial[3] += virial_v4[3];
+  virial[4] += virial_v4[4];
+  virial[5] += virial_v4[5];
   /* for (i = 0; i < 64; i ++){ */
   /*   if (_MYID == i){ */
   /*     simd_print_doublev4(eng_vdwl_v4); */
@@ -730,13 +742,13 @@ void pair_lj_cut_sunway_compute_para_vec(compute_param_t *pm){
   /*   athread_syn(ARRAY_SCOPE, 0xffff); */
   /* } */
   reg_reduce_inplace_doublev4(eng_virial, 2);
-  lwpf_start(GST);
+  //lwpf_start(GST);
   if (_MYID == 0){
     pe_put(&(pm->eng_vdwl), eng_vdwl, sizeof(double) * 8);
     pe_syn();
   }
-  lwpf_stop(GST);
-  lwpf_stop(ALL);
+  //lwpf_stop(GST);
+  //lwpf_stop(ALL);
 }
 
 #endif
